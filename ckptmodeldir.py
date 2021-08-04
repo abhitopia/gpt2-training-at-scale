696f12db53e73fd72b4e163bd8be05e079d2c38c
abhitopia <abhitopia@isme.com>
2021-08-04 06:28:44 +0100

    modified ckpoint conversion file

--- ckptmodeldir.py
+++ ckptmodeldir.py
@@ -3,12 +3,13 @@ import torch
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
-ckpt = torch.load("models/se_gpt2_distil/checkpoint.pth", map_location="cpu")
+output_path = 'models/zd_cs_gpt2_v1/model_dir'
+ckpt = torch.load("models/zd_cs_gpt2_v1/checkpoint.pth", map_location="cpu")
 
 state_dict = ckpt['model_state_dict']
 config = GPT2Config.from_dict(ckpt['config'])
 model = GPT2LMHeadModel(config=config)
 model.load_state_dict(state_dict=state_dict, strict=True)
-model.save_pretrained('models/sedistilfastwikiv1')
-tokenizer.save_pretrained('models/sedistilfastwikiv1')
+model.save_pretrained(output_path)
+tokenizer.save_pretrained(output_path)
 doSomething = 1
